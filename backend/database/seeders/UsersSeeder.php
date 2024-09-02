a940bb47279127acbd2661e9c67f2f9e52f4580f
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-09-02 22:24:37 +0700

    feat: add database seeder

---
+++ backend/database/seeders/UsersSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Seeder;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'username' => 'fufufuad',
+                'email' => 'fuad@gmail.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('1234fuad'),
+                'role' => 'buyer',
+                'firstname' => 'Fuad',
+                'lastname' => 'Grimaldi',
+                'img_profile' => null,
+                'phone_number' => null,
+                'gender' => null,
+                'birthdate' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'username' => 'team2',
+                'email' => 'team2@gmail.com',
+                'email_verified_at' => now(),
+                'password' => bcrypt('12345678'),
+                'role' => 'seller',
+                'firstname' => 'Team',
+                'lastname' => '2',
+                'img_profile' => null,
+                'phone_number' => null,
+                'gender' => null,
+                'birthdate' => null,
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ]);
+    }
+}
