431def11e57f3cb3c3d8d8edca0e765f1c1f22cb
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-09-05 01:41:04 +0700

    fix:api route product

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\DetailSystemRequireMentController;
-use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductControllerDev;
 
 # PUBLIC
 Route::post('/login', [UserController::class, 'login']);
@@ -15,13 +15,11 @@ Route::post('/users/register', [UserController::class, 'register']);
 Route::middleware(['auth-token'])->group(function () {
     Route::get('/users/profile', [UserController::class, 'getProfile']);
     Route::delete('/users/logout', [UserController::class, 'logout']);
-
+    
+    Route::resource('product', ProductControllerDev::class);
     Route::resource('system-recuirement', SystemRequireMentController::class);
     Route::resource('detail-system-requirement', DetailSystemRequireMentController::class);
     Route::post('/users/logout', [UserController::class, 'logout']); 
-    Route::get('/products', [ProductController::class, 'getAllProducts']);
-    Route::get('/products/{id}', [ProductController::class, 'getProduct']);
-    Route::post('/products/add', [ProductController::class, 'createProduct']);
 });
 
 
