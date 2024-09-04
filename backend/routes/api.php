adb87c4970ccab0f40fc3dec11602df94af5a645
Fuad Grimaldi <fuadgrimaldi123@gmail.com>
2024-09-04 15:18:59 +0700

    feat: add route api product

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 # PUBLIC
 Route::post('/login', [UserController::class, 'login']);
@@ -12,5 +13,9 @@ Route::post('/users/register', [UserController::class, 'register']);
 Route::middleware(['auth-token'])->group(function () {
     Route::get('/users/profile', [UserController::class, 'getProfile']);
     Route::post('/users/logout', [UserController::class, 'logout']); 
+    Route::get('/products', [ProductController::class, 'getAllProducts']);
+    Route::get('/products/{id}', [ProductController::class, 'getProduct']);
+    Route::post('/products/add', [ProductController::class, 'createProduct']);
 });
+
 
