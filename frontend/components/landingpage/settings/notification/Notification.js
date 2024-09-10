14c31f700249825dbd0f0a371518d1765cc5fb1c
firyalhuwaidaa <firyalhuwaidaa@gmail.com>
2024-09-11 00:04:43 +0700

    feat: Slicing UI Notification

--- frontend/components/landingpage/settings/notification/Notification.js
+++ frontend/components/landingpage/settings/notification/Notification.js
@@ -1,9 +1,41 @@
-export default function Wishlist() {
+import NotificationItem from "./NotificationItem";
+
+const initialNotificationItems = [
+  {
+    id: 1,
+    title: "Order has been received",
+    image: "/images/detail1.png",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id condimentum semper libero tempor egestas facilisi quam accumsan, aliquam. Ac nunc eget interdum pellentesque et tortor. Pretium risus viverra mauris aliquam adipiscing ut pretium. Iaculis ipsum gravida tempus, eget pellentesque venenatis purus.",
+  },
+  {
+    id: 2,
+    title: "Order has been received",
+    image: "/images/detail1.png",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id condimentum semper libero tempor egestas facilisi quam accumsan, aliquam. Ac nunc eget interdum pellentesque et tortor. Pretium risus viverra mauris aliquam adipiscing ut pretium. Iaculis ipsum gravida tempus, eget pellentesque venenatis purus.",
+  },
+  {
+    id: 3,
+    title: "Order has been received",
+    image: "/images/detail1.png",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id condimentum semper libero tempor egestas facilisi quam accumsan, aliquam. Ac nunc eget interdum pellentesque et tortor. Pretium risus viverra mauris aliquam adipiscing ut pretium. Iaculis ipsum gravida tempus, eget pellentesque venenatis purus.",
+  },
+  {
+    id: 4,
+    title: "Order has been received",
+    image: "/images/detail1.png",
+    desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Id condimentum semper libero tempor egestas facilisi quam accumsan, aliquam. Ac nunc eget interdum pellentesque et tortor. Pretium risus viverra mauris aliquam adipiscing ut pretium. Iaculis ipsum gravida tempus, eget pellentesque venenatis purus.",
+  },
+];
+
+export default function Notification() {
   return (
-    <div>
-      <h2 className="text-2xl font-semibold mb-4">My Notification</h2>
-      <p>Here you can view and manage your notification items.</p>
-      {/* Add your wishlist content here */}
+    <div className="w-full pb-10 p-6 border border-white">
+      <div className="flex items-center mx-0 lg:mx-6 border-b border-[#808080]">
+        <h1 className="text-2xl font-bold mb-3 lg:mb-6 mt-6">Notification</h1>
+      </div>
+      {initialNotificationItems.map((item) => (
+        <NotificationItem key={item.id} item={item} />
+      ))}
     </div>
   );
 }
